af302fe6b9112ba1f526f1306b691f1ef7db008f
jhonatasal <jhonatasa_s@hotmail.com>
2022-04-07 10:58:59 -0300

    correção log direct

--- src/main/java/com/rentgame/resources/GameRabbitMqResourceDirect.java
+++ src/main/java/com/rentgame/resources/GameRabbitMqResourceDirect.java
@@ -16,7 +16,6 @@ import com.rentgame.services.interfaces.RabbitmqDirectService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping(path = GameRabbitMqResourceDirect.URI)
 public class GameRabbitMqResourceDirect {
@@ -30,19 +29,20 @@ public class GameRabbitMqResourceDirect {
 
 	@PostMapping("/1")
 	public void enviarMessageFila1(@Valid @RequestBody GameRequest request) {
-		log.info("Enviando mensagem para routingkey1");
+		System.out.println("Enviando mensagem para routingkey1");
+		
 		rabbitmqDirectService.sendMessageDirectKey(request, mapNames.get("routingkey1"));
 	}
 
 	@PostMapping("/2")
 	public void enviarMessageFila2(@Valid @RequestBody GameRequest request) {
-		log.info("Enviando mensagem para routingkey2");
+		System.out.println("Enviando mensagem para routingkey2");
 		rabbitmqDirectService.sendMessageDirectKey(request, mapNames.get("routingkey2"));
 	}
 
 	@PostMapping("/3")
 	public void enviarMessageFila3(@Valid @RequestBody GameRequest request) {
-		log.info("Enviando mensagem para routingkey3");
+		System.out.println("Enviando mensagem para routingkey3");
 		rabbitmqDirectService.sendMessageDirectKey(request, mapNames.get("routingkey3"));
 	}
 }
